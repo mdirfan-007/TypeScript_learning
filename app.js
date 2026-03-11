9ba551b8b9eba8e0ba424c31a021a31c5b67c35a
MD IRFAN <mdimrfat007@gmail.com>
2026-03-11 13:22:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let arr = ['harsh', 2];
+var statusCode;
+(function (statusCode) {
+    statusCode[statusCode["success"] = 200] = "success";
+    statusCode[statusCode["notFound"] = 404] = "notFound";
+    statusCode[statusCode["serverError"] = 500] = "serverError";
+})(statusCode || (statusCode = {}));
+statusCode.notFound;
+let a;
+a = 11;
+a = 11;
+a = 23223;
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,17 @@
+let arr: [string,number] = ['harsh',2]
+
+
+enum statusCode{
+    success = 200,
+    notFound = 404,
+    serverError = 500
+}
+
+statusCode.notFound
+
+
+let a: number;
+
+a=11
+a=11
+a=23223
