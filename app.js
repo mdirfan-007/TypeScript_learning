e6a2ea1b5c5e5e167059aa9eb085d768c5070ebc
MD IRFAN <mdimrfat007@gmail.com>
2026-03-12 15:01:50 +0530

    12--3-26

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let arr = ['harsh', 2];
-var statusCode;
-(function (statusCode) {
-    statusCode[statusCode["success"] = 200] = "success";
-    statusCode[statusCode["notFound"] = 404] = "notFound";
-    statusCode[statusCode["serverError"] = 500] = "serverError";
-})(statusCode || (statusCode = {}));
-statusCode.notFound;
-let a;
-a = 11;
-a = 11;
-a = 23223;
-//# sourceMappingURL=app.js.map
+var User = /** @class */ (function () {
+    function User() {
+        this.name = 'harsh';
+        this.age = 22;
+        this.village = 'sangamner';
+    }
+    return User;
+}());
+var user1 = new User();
+console.log(user1);
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person;
+}());
+var b1 = new Person('harsh', 22);
+console.log(b1);
+function getMan(name, age, gender) {
+    if (gender === void 0) { gender = 'not to say'; }
+    console.log(name, age, gender);
+}
+getMan('Irfan', 26, 'Male');
+getMan('Arthor', 22);
+function add() {
+    var trr = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        trr[_i] = arguments[_i];
+    }
+    console.log(trr);
+}
+add(1, 2, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8);
--- app.ts
+++ app.ts
@@ -1,17 +1,35 @@
-let arr: [string,number] = ['harsh',2]
 
 
-enum statusCode{
-    success = 200,
-    notFound = 404,
-    serverError = 500
+class User{
+    name='harsh'
+    age= 22
+    village= 'sangamner'
 }
 
-statusCode.notFound
+var user1 = new User()
+console.log(user1);
 
 
-let a: number;
+class Person {
+  constructor(public name: string, private age: number) {}
+}
 
-a=11
-a=11
-a=23223
+let b1 = new Person('harsh', 22);
+console.log(b1);
+
+
+function getMan (name: string, age: number, gender: string='not to say'){
+    console.log(name, age, gender);
+} 
+
+getMan('Irfan',26,'Male')
+getMan('Arthor',22)
+
+
+function add(...trr:number[]){
+    console.log(trr);
+    
+
+}
+
+add(1,2,3,3,3,3,3,3,3,4,5,6,7,8)
