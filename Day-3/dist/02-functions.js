0aa85f979d590022c833ed888c1e11c02a9ef344
MD IRFAN <mdimrfat007@gmail.com>
2026-04-20 13:17:43 +0530

    Add TypeScript learning code

--- Day-3/dist/02-functions.js
+++ Day-3/dist/02-functions.js
@@ -9,3 +9,22 @@ function greet(name, greeting) {
 }
 const greetingMessage = greet('Alice', 'Welcome to TypeScript!');
 console.log(greetingMessage);
+const add1 = (a, b) => a + b;
+const result1 = add1(22, 88);
+console.log(result1);
+function greet1(name, age, age2) {
+    console.log(name, age, age2);
+}
+greet1("Irfan");
+greet1("Irfan", 22, 24);
+function greet3(name = "Guest", name2) {
+    console.log("Hello " + name);
+}
+greet3('Irfan');
+const user = {
+    name: "Irfan",
+    greet() {
+        console.log("Hi " + this.name);
+    }
+};
+user.greet();
--- Day-3/src/02-functions.ts
+++ Day-3/src/02-functions.ts
@@ -11,3 +11,38 @@ function greet(name:string , greeting:string ): string {
 
 const greetingMessage = greet('Alice', 'Welcome to TypeScript!');
 console.log(greetingMessage);
+
+const add1 = (a: number, b: number): number => a+b;
+
+const result1 = add1(22,88);
+console.log(result1);
+
+function greet1(name: string, age?: number, age2?: number): void {
+  console.log(name, age, age2);
+}
+
+greet1("Irfan");       
+greet1("Irfan", 22,24);   
+
+
+function greet3(name: string= "Guest",name2?:string): void {
+  console.log("Hello " + name);
+}
+
+greet3('Irfan'); 
+
+// Interface with Method
+
+interface Person {
+  name: string;
+  greet(): void;
+}
+
+const user: Person = {
+  name: "Irfan",
+  greet() {
+    console.log("Hi " + this.name);
+  }
+};
+
+user.greet();
